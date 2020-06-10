4803508735632864c54fa86e118642fd3c063542
Victor Hugo <vhugoro@gmail.com>
2020-06-10 16:15:23 -0300

    Criando PetPAss

---
+++ src/main/java/br/com/fiap/PetPass/PetPassApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.PetPass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetPassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetPassApplication.class, args);
+	}
+
+}
