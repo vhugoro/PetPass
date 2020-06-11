5f864568709cf6b7a69335eccc55135ca815fd7e
Victor Hugo <vhugoro@gmail.com>
2020-06-10 22:56:49 -0300

    transformando plano em enum

---
+++ src/main/java/br/com/fiap/PetPass/enums/PlanoEnum.java
@@ -0,0 +1,21 @@
+package br.com.fiap.PetPass.enums;
+
+public enum PlanoEnum {
+	BRONZE(0),
+	PRATA(1),
+	OURO(2);
+	
+	private Integer plano;
+	
+	private PlanoEnum(Integer plano) {
+		this.plano = plano;
+	}
+	
+	public Integer getPlano() {
+		return plano;
+	}
+	
+	public void setPlano(Integer plano) {
+		this.plano = plano;
+	}
+}
